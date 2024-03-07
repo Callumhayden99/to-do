05dfdce4e3251ac08846e06f1c6e7bf606a773b9
callumhayden <callum.hayden@gmail.com>
2024-03-07 15:00:33 +0000

    fixing error

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     const token = jwt.sign(
       { id: user.id, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: "60s" }
+      { expiresIn: "8h" }
     );
 
     // Instead of returning the token directly,
